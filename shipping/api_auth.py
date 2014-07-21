57a307f9f77ea4f2a97ed048dffdba56f4291ba9
Esa Varemo <esa@kuivanto.fi>
2014-07-21 22:11:04 +0300

    Changed config path and making shop name configurable

--- shipping/api_auth.py
+++ shipping/api_auth.py
@@ -1,17 +1,26 @@
 import shopify
+import os.path
+
+path = os.path.join("shipping", "config")
 
 def get_api_key():
     apikey = ""
-    with open("auth_apikey", 'r', encoding="utf-8") as file_apikey:
+    with open(os.path.join(path, "auth_apikey"), 'r', encoding="utf-8") as file_apikey:
         apikey = file_apikey.readline()
     return apikey
 
 def get_password():
     password = ""
-    with open("auth_password", 'r', encoding="utf-8") as file_password:
+    with open(os.path.join(path, "auth_password"), 'r', encoding="utf-8") as file_password:
         password = file_password.readline()
     return password
 
+def get_shopname():
+    shopname = ""
+    with open(os.path.join(path, "shopname"), 'r', encoding="utf-8") as file_shopname:
+        shopname = file_shopname.readline()
+    return shopname
+
 def create_session():
-    shop_url = "https://%s:%s@SHOP_NAME.myshopify.com/admin" % (get_api_key(), get_password())
+    shop_url = "https://%s:%s@%s.myshopify.com/admin" % (get_api_key(), get_password(), get_shopname())
     shopify.ShopifyResource.set_site(shop_url)
