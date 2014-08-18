6a41203e1db0cf7fe1a84320d2e7b961d55c4408
Esa Varemo <esa@kuivanto.fi>
2014-08-18 23:26:59 +0300

    Expanding distance calculation

--- shipping/api_distance.py
+++ shipping/api_distance.py
@@ -10,16 +10,16 @@ from .exceptions import InvalidLocationException
 baseurl = "http://maps.googleapis.com/maps/api/distancematrix/json?"
 
 
-def url_from_json(location):
+def url_from_json(location_json):
     """
     Encode a location represented by a dictionary of values into a
     string that can be passed as a GMaps parameter
-    :param location: Dictionary that represents a location
-    :type location: dict
+    :param location_json: Dictionary that represents a location
+    :type location_json: str
     :return: An url-encoded (with plus-signs) string representing the location
     :rtype: str
     """
-    location = json.loads(location)
+    location = json.loads(location_json)
     full_address = ""
     full_address += location['address1'] + ", "
     full_address += (location['address2'] + ", ") if len(location['address2']) > 0 else ''
@@ -30,22 +30,31 @@ def url_from_json(location):
     return quote_plus(full_address)
 
 
-def get_distance(origin, destination):
+def url_from_string(location_string):
+    """
+    Encode a location represented by a string into a url-form that can be passed to GMaps
+    :param location_string: String to be URL-encoded
+    :type location_string: str
+    :return: An URL-encoded (with plus-signs) string representing the location
+    :rtype: str
+    """
+    return quote_plus(location_string)
+
+
+def get_distance_url_url(origin_url, destination_url):
     """
     Do a request to GMaps for a distance between two points
-    :param origin: First of the two points in JSON
-    :type origin: str
-    :param destination: Second of the two points in JSON
-    :type destination: str
+    :param origin_url: First of the two points in JSON
+    :type origin_url: str
+    :param destination_url: Second of the two points in JSON
+    :type destination_url: str
     :return: distance between locations in kilometers, rounded up
     :rtype: int
     """
-    origin_url = url_from_json(origin)
-    dest_url = url_from_json(destination)
 
     print("Requesting distance")
 
-    request_url = baseurl + "origins=" + origin_url + "&destinations=" + dest_url
+    request_url = baseurl + "origins=" + origin_url + "&destinations=" + destination_url
     response_json = urlopen(request_url).read().decode('utf-8')
     response = json.loads(response_json)
 
@@ -55,3 +64,46 @@ def get_distance(origin, destination):
         raise InvalidLocationException
 
     return ceil(int(response['rows'][0]['elements'][0]['distance']['value'])/1000.0)
+
+
+def get_distance_json_json(origin_json, destination_json):
+    """
+    Get the distance between two points that are both represented in JSON
+    :param origin_json: JSON-string for the origin point
+    :type origin_json: str
+    :param destination_json: JSON-string for the destination point
+    :type destination_json: str
+    :return: distance as returned by :method: get_distance_url_url
+    """
+    get_distance_url_url(url_from_json(origin_json), url_from_json(destination_json))
+
+
+def get_distance_string_json(origin_string, destination_json):
+    """
+    Get the distance between two points where origin is in string form and destination in JSON
+    :param origin_string: string for the origin point
+    :type origin_string: str
+    :param destination_json: JSON-string for the destination point
+    :type destination_json: str
+    :return: distance as returned by :method: get_distance_url_url
+    """
+    get_distance_url_url(url_from_string(origin_string), url_from_json(destination_json))
+
+
+def shortest_distance_string_json(origin_strings, destination_json):
+    """
+    Find the shortest pair from a group of origins and a destination
+    :param origin_strings: Array of origins
+    :type origin_strings: array of strings
+    :param destination_json: destination in JSON form
+    :type destination_json: str
+    :return: Distance between the closest two points
+    """
+    min_dist = None
+
+    for origin in origin_strings:
+        dist = get_distance_string_json(origin, destination_json)
+        if min_dist == None or dist < min_dist:
+            min_dist = dist
+
+    return min_dist
